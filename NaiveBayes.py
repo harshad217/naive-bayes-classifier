5ee2da3cde013e40dfc3082691eb79b88203e424
harshad217 <harshadr@buffalo.edu>
2015-12-05 17:29:30 -0500

    modify predict def

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -6,37 +6,43 @@ import math
 from sklearn.naive_bayes import GaussianNB
 
 def calculateGnb(x,mean,var):
-    first_term = float(1/math.sqrt(2 * math.pi * var))
+    first_term = float(1)/float(math.sqrt(2 * math.pi * var))
     num = math.pow((x - mean),2)
     denom = (2 * var)
-    fract = -float(num / denom)
-    second_term = math.pow(math.e,fract)
-    gnb = first_term * second_term
+    fract = -( float(num) / float(denom) )
+    second_term = math.exp(fract)
+    gnb = float(first_term) * float(second_term)
     return gnb
 
 def Main():
     matrix = np.loadtxt('data/project3_dataset1.txt')
+    
     original_matrix = matrix
-    indexes = np.random.permutation(matrix.shape[0])
+    # indexes = np.random.permutation(matrix.shape[0])
+    indexes = range(matrix.shape[0])
     partition = int(len(matrix) * 0.7)
     training_indexes, testing_indexes = indexes[:partition], indexes[partition:]
+    print 'train indexes = ', training_indexes
     training, testing = matrix[training_indexes,:], matrix[testing_indexes,:]
     # print len(training), len(testing)
 
     training_truths_index = len(training[0]) - 1
     training_truths = training[:,training_truths_index]
     print 'length of train truths = ',len(training_truths)
-    # training = np.delete(training,training_truths_index,1)
+    training_matrix = training
+    training = np.delete(training,training_truths_index,1)
     print 'length of training columns = ', len(training[0])
 
     testing_truths_index = len(testing[0]) - 1
     testing_truths = testing[:,testing_truths_index]
     print 'length of test truths = ',len(testing_truths)
-    # testing = np.delete(testing,testing_truths_index,1)
+    testing = np.delete(testing,testing_truths_index,1)
     print 'length of test columns = ', len(testing[0])
 
-    label0_mean_list = np.zeros(shape=(1,len(training[0])-1))
-    label1_mean_list = np.zeros(shape=(1,len(training[0])-1))
+    # label0_mean_list = np.zeros(shape=(1,len(training[0])-1))
+    label0_mean_list = [ ]
+    # label1_mean_list = np.zeros(shape=(1,len(training[0])-1))
+    label1_mean_list = [ ]
 
     label0_matrix = np.empty(shape=(0,len(training[0])-1))
     label1_matrix = np.empty(shape=(0,len(training[0])-1))
@@ -44,19 +50,20 @@ def Main():
     prior0 = 0
     prior1 = 0
     total_prior = len(training)
-    for row in training:
-        if row[len(training[0])-1] == 0:
-            to_add_row = row[:len(row)-1]
-            label0_matrix = np.vstack([label0_matrix,to_add_row])
-            prior0 += 1
-        elif row[len(training[0])-1] == 1:
-            to_add_row = row[:len(row)-1]
-            label1_matrix = np.vstack([label1_matrix,to_add_row])
-            prior1 += 1
+    print 'shit = ',training_matrix[:,len(training_matrix[0])-1]
+    print 'shit complete'
+    '''create label0 data matrix and label1 data matrix'''
+    label0_matrix = training_matrix[np.ix_(training_matrix[:,len(training_matrix[0])-1] == 0, range(len(training_matrix[0])-1))]
+
+    '''create label0 data matrix and label1 data matrix'''
+    label1_matrix = training_matrix[np.ix_(training_matrix[:,len(training_matrix[0])-1] == 1, range(len(training_matrix[0])-1))]
+
+    prior0 = len(label0_matrix)
+    prior1 = len(label1_matrix)
 
     print 'prior label-0 probability = ', prior0, '/',total_prior
-    prior0_probability = float(float(prior0)/float(total_prior))
-    prior1_probability = float(float(prior1)/float(total_prior))
+    prior0_probability = float(prior0)/float(total_prior)
+    prior1_probability = float(prior1)/float(total_prior)
 
     print 'len of matrix of label 0s',len(label0_matrix)
     print 'len of matrix of label 1s',len(label1_matrix)
@@ -65,36 +72,41 @@ def Main():
     '''init label0 mean list'''
     j=0
     for each_column in label0_matrix.T:
-        label0_mean_list[0,j] = np.mean(each_column)
+        # label0_mean_list[j] = np.mean(each_column)
+        label0_mean_list.insert(j,np.mean(each_column))
         j = j + 1
 
     '''init label1 mean list'''
     j=0
     for each_column in label1_matrix.T:
-        label1_mean_list[0,j] = np.mean(each_column)
+        # label1_mean_list[j] = np.mean(each_column)
+        label1_mean_list.insert(j,np.mean(each_column))
         j = j + 1
 
-    print label0_mean_list, 'len of label0 means = ', len(label0_mean_list[0])
-    print label1_mean_list, 'len of label1 means = ', len(label1_mean_list[0])
+    print label0_mean_list, 'len of label0 means = ', len(label0_mean_list)
+    print label1_mean_list, 'len of label1 means = ', len(label1_mean_list)
 
-    label0_var_list = np.zeros(shape=(1,len(training[0])-1))
-    label1_var_list = np.zeros(shape=(1,len(training[0])-1))
+    # label0_var_list = np.zeros(shape=(1,len(training[0])-1))
+    label0_var_list = [ ]
+    # label1_var_list = np.zeros(shape=(1,len(training[0])-1))
+    label1_var_list = [ ]
 
     '''Init label0 variance list'''
     j=0
     for each_column in label0_matrix.T:
-        label0_var_list[0,j] = np.var(each_column)
+        # label0_var_list[j] = np.var(each_column)
+        label0_var_list.insert(j, np.var(each_column))
         j = j + 1
 
     '''Init label1 variance list'''
     j=0
     for each_column in label1_matrix.T:
-        label1_var_list[0,j] = np.var(each_column)
+        # label1_var_list[j] = np.var(each_column)
+        label1_var_list.insert(j, np.var(each_column))
         j = j + 1
 
-    print 'label0 variance list length = ',len(label0_var_list[0])
-    print 'label1 variance list length = ',len(label1_var_list[0])
-    print label0_var_list
+    print 'label0 variance list length = ',len(label0_var_list)
+    print 'label1 variance list length = ',len(label1_var_list)
 
     predicted_results = [ ]
 
@@ -102,32 +114,33 @@ def Main():
     for i in range(len(testing)):
         label0_gnb_product = prior0_probability
         label1_gnb_product = prior1_probability
-        for j in range(len(testing[0])-1):
+        for j in range(len(testing[0])):
             #calculate gnb for each feature for label1 and label0 and multiply all
-            gnb0 = calculateGnb(testing[i,j],label0_mean_list[0,j],label0_var_list[0,j])
+            gnb0 = calculateGnb(testing[i,j],label0_mean_list[j],label0_var_list[j])
             label0_gnb_product = label0_gnb_product * gnb0
-            gnb1 = calculateGnb(testing[i,j],label1_mean_list[0,j],label1_var_list[0,j])
+            gnb1 = calculateGnb(testing[i,j],label1_mean_list[j],label1_var_list[j])
             label1_gnb_product = label1_gnb_product * gnb1
+            # print 'row:',i,'element:',j,'= ', testing[i,j]
 
-        if gnb0 > gnb1:
+        if label0_gnb_product > label1_gnb_product:
             predicted_results.insert(i,0)
         else:
             predicted_results.insert(i,1)
 
     print len(predicted_results)
     print len(testing_truths)
+
     accuracy = 0
     for i in range(len(predicted_results)):
         if predicted_results[i] == testing_truths[i]:
             accuracy += 1
 
-    print 'accuracy = ',float(float(accuracy)/float(len(predicted_results)))
+    print 'accuracy = ',float(accuracy)/float(len(predicted_results))
 
     clf = GaussianNB()
-    training_to_pass = np.delete(training,axis=1,obj=len(training[0])-1)
-    clf.fit(training_to_pass, training_truths)
-    testing_to_pass = np.delete(testing,axis=1,obj=len(testing[0])-1)
-    pack_results = clf.predict(testing_to_pass)
+    clf.fit(training, training_truths)
+    # testing_to_pass = np.delete(testing,axis=1,obj=len(testing[0])-1)
+    pack_results = clf.predict(testing)
 
     acc = 0
     for i in range(len(pack_results)):
@@ -135,5 +148,6 @@ def Main():
             acc += 1
 
     print 'actual accuracy = ', float(float(acc)/float(len(testing_truths)))
+
 if __name__ == '__main__':
     Main()
